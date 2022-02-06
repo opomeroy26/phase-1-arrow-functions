208424455fc9307adc01c15726a80221bcc5fb4b
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-06 11:41:06 -0800

    commit tree 8060fa323e82cefd089294e9ce655f431a456e6f

---
+++ index.js
@@ -0,0 +1,15 @@
+// const divide = function () {
+//     return 2000/100;
+// }
+
+// const square = x => x*x;
+
+// const add = (a,b) => a + b;
+
+
+
+const divide = () => 2000/100;
+
+const square = x => x*x;
+
+const add = (a, b) => a + b;
